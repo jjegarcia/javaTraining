94801b7b5e41c74456d0c4b3a692b2ca935864b7
Jenaro Garcia <jgarc609@ford.com>
2020-10-27 18:06:27 +0000

    using interfaces easily created default signatures

--- src/calculator/MathProcessing.java
+++ src/calculator/MathProcessing.java
@@ -4,4 +4,11 @@ public interface MathProcessing {
     String SEPARATOR =" ";
     String getKeyWord();
     double doCalculation(double leftVal, double rightVal);
+    default String getFormattedOutput(){
+        return null;
+    }
+    default double test(double leftVal, double rightVal) {
+        double test= leftVal+rightVal;
+    return test ;
+    }
 }
